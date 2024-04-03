ee3226d417235c087bbb1bef8b4228d3a26b6c1c
Hsu0734 <xuhwmy@gmail.com>
2024-04-03 17:28:47 +0200

    0403

---
+++ 02_SWMM_optimization/01_optimization_rate.py
@@ -0,0 +1,251 @@
+'''
+PySWMM + Pymoo Code Practice
+A simple optimization case of LID (BR) area
+Objective: Min(total Cost) & Max(Reduction of runoff volume) & & Max(Reduction of peak volume)
+Author: Hanwen Xu
+Version: 1
+Date: Nov 01, 2023
+'''
+
+from pyswmm import Simulation, Subcatchments, LidGroups, SystemStats
+import numpy as np
+
+
+# setting variables
+
+sim = Simulation(r'../GuanyaoshanNJ.inp')
+lid_on_subs = LidGroups(sim)
+
+# define MOO problem
+from pymoo.core.problem import ElementwiseProblem
+
+class MyProblem(ElementwiseProblem):
+
+    def __init__(self, lid_on_subs, **kwargs):
+        super().__init__(n_var=30,
+                         n_obj=3,
+                         n_ieq_constr=1,
+                         xl=np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]),
+                         xu=np.array([13500, 8100, 7600, 9300, 11900, 5100, 11700, 13000, 9100, 6400, 18500, 6600, 5400,
+                                      16700, 11400, 17200, 8000, 12400, 10500, 8200, 7600, 14700, 9500, 8200, 19800, 26300, 8100, 19800, 5600, 12300]),
+                         **kwargs)
+        self.lid_on_subs = lid_on_subs
+
+    def _evaluate(self, x, out, *args, **kwargs):
+        S1_BR = x[0]
+        S1_1_BR = x[1]
+        S2_BR = x[2]
+        S2_1_BR = x[3]
+        S3_BR = x[4]
+        S3_1_BR = x[5]
+        S4_BR = x[6]
+        S4_1_BR = x[7]
+        S5_BR = x[8]
+        S5_1_BR = x[9]
+        S6_BR = x[10]
+        S6_1_BR = x[11]
+        S6_2_BR = x[12]
+        S7_BR = x[13]
+        S7_1_BR = x[14]
+        S8_BR = x[15]
+        S9_BR = x[16]
+        S10_BR = x[17]
+        S10_1_BR = x[18]
+        S11_BR = x[19]
+        S11_1_BR = x[20]
+        S12_BR = x[21]
+        S12_1_BR = x[22]
+        S12_2_BR = x[23]
+        S12_3_BR = x[24]
+        S12_4_BR = x[25]
+        S13_BR = x[26]
+        S13_1_BR = x[27]
+        S14_BR = x[28]
+        S14_1_BR = x[29]
+
+        cost_function = 0.04 * (S1_BR + S1_1_BR + S2_BR + S2_1_BR + S3_BR + S3_1_BR + S4_BR + S4_1_BR + S5_BR + S5_1_BR +
+                                S6_BR + S6_1_BR + S6_2_BR + S7_BR + S7_1_BR + S8_BR + S9_BR + S10_BR + S10_1_BR + S11_BR +
+                                S11_1_BR + S12_BR + S12_1_BR + S12_2_BR + S12_3_BR + S12_4_BR + S13_BR + S13_1_BR + S14_BR + S14_1_BR)
+        total_runoff_function = compute_runoff(S1_BR, S1_1_BR, S2_BR, S2_1_BR, S3_BR, S3_1_BR, S4_BR, S4_1_BR, S5_BR, S5_1_BR,
+                                S6_BR, S6_1_BR, S6_2_BR, S7_BR, S7_1_BR, S8_BR, S9_BR, S10_BR, S10_1_BR, S11_BR,
+                                S11_1_BR, S12_BR, S12_1_BR, S12_2_BR, S12_3_BR, S12_4_BR, S13_BR, S13_1_BR, S14_BR, S14_1_BR)
+        peak_runoff_fuction = compute_peak_runoff(S1_BR, S1_1_BR, S2_BR, S2_1_BR, S3_BR, S3_1_BR, S4_BR, S4_1_BR, S5_BR, S5_1_BR,
+                                S6_BR, S6_1_BR, S6_2_BR, S7_BR, S7_1_BR, S8_BR, S9_BR, S10_BR, S10_1_BR, S11_BR,
+                                S11_1_BR, S12_BR, S12_1_BR, S12_2_BR, S12_3_BR, S12_4_BR, S13_BR, S13_1_BR, S14_BR, S14_1_BR)
+
+        # constrain function
+        g1 = 4317 - (S1_BR + S1_1_BR + S2_BR + S2_1_BR + S3_BR + S3_1_BR + S4_BR + S4_1_BR + S5_BR + S5_1_BR + S6_BR + S6_1_BR + \
+             S6_2_BR + S7_BR + S7_1_BR + S8_BR + S9_BR + S10_BR + S10_1_BR + S11_BR + S11_1_BR + S12_BR + S12_1_BR + S12_2_BR + \
+             S12_3_BR + S12_4_BR + S13_BR + S13_1_BR + S14_BR + S14_1_BR)
+
+        out["F"] = [cost_function, total_runoff_function, peak_runoff_fuction]
+        out["G"] = [g1]
+
+def compute_runoff(S1_BR, S1_1_BR, S2_BR, S2_1_BR, S3_BR, S3_1_BR, S4_BR, S4_1_BR, S5_BR, S5_1_BR,
+                   S6_BR, S6_1_BR, S6_2_BR, S7_BR, S7_1_BR, S8_BR, S9_BR, S10_BR, S10_1_BR, S11_BR,
+                   S11_1_BR, S12_BR, S12_1_BR, S12_2_BR, S12_3_BR, S12_4_BR, S13_BR, S13_1_BR, S14_BR, S14_1_BR):
+    sim = Simulation(r'../GuanyaoshanNJ.inp')
+    system_routing = SystemStats(sim)
+
+    Time_stamps = [] # add time series
+    Runoff = [] # creat a set for runoff data collection
+
+    lid_on_subs.__getitem__('S1').__getitem__(0).unit_area = S1_BR
+    lid_on_subs.__getitem__('S1-1').__getitem__(0).unit_area = S1_1_BR
+    lid_on_subs.__getitem__('S2').__getitem__(0).unit_area = S2_BR
+    lid_on_subs.__getitem__('S2-1').__getitem__(0).unit_area = S2_1_BR
+    lid_on_subs.__getitem__('S3').__getitem__(0).unit_area = S3_BR
+    lid_on_subs.__getitem__('S3-1').__getitem__(0).unit_area = S3_1_BR
+    lid_on_subs.__getitem__('S4').__getitem__(0).unit_area = S4_BR
+    lid_on_subs.__getitem__('S4-1').__getitem__(0).unit_area = S4_1_BR
+    lid_on_subs.__getitem__('S5').__getitem__(0).unit_area = S5_BR
+    lid_on_subs.__getitem__('S5-1').__getitem__(0).unit_area = S5_1_BR
+    lid_on_subs.__getitem__('S6').__getitem__(0).unit_area = S6_BR
+    lid_on_subs.__getitem__('S6-1').__getitem__(0).unit_area = S6_1_BR
+    lid_on_subs.__getitem__('S6-2').__getitem__(0).unit_area = S6_2_BR
+    lid_on_subs.__getitem__('S7').__getitem__(0).unit_area = S7_BR
+    lid_on_subs.__getitem__('S7-1').__getitem__(0).unit_area = S7_1_BR
+    lid_on_subs.__getitem__('S8').__getitem__(0).unit_area = S8_BR
+    lid_on_subs.__getitem__('S9').__getitem__(0).unit_area = S9_BR
+    lid_on_subs.__getitem__('S10').__getitem__(0).unit_area = S10_BR
+    lid_on_subs.__getitem__('S10-1').__getitem__(0).unit_area = S10_1_BR
+    lid_on_subs.__getitem__('S11').__getitem__(0).unit_area = S11_BR
+    lid_on_subs.__getitem__('S11-1').__getitem__(0).unit_area = S11_1_BR
+    lid_on_subs.__getitem__('S12').__getitem__(0).unit_area = S12_BR
+    lid_on_subs.__getitem__('S12-1').__getitem__(0).unit_area = S12_1_BR
+    lid_on_subs.__getitem__('S12-2').__getitem__(0).unit_area = S12_2_BR
+    lid_on_subs.__getitem__('S12-3').__getitem__(0).unit_area = S12_3_BR
+    lid_on_subs.__getitem__('S12-4').__getitem__(0).unit_area = S12_4_BR
+    lid_on_subs.__getitem__('S13').__getitem__(0).unit_area = S13_BR
+    lid_on_subs.__getitem__('S13-1').__getitem__(0).unit_area = S13_1_BR
+    lid_on_subs.__getitem__('S14').__getitem__(0).unit_area = S14_BR
+    lid_on_subs.__getitem__('S14-1').__getitem__(0).unit_area = S14_1_BR
+
+
+    for step in sim:
+        Time_stamps.append(sim.current_time)
+        runoff_stats = system_routing.runoff_stats
+        runoff_volume = runoff_stats.__getitem__('runoff')
+        Runoff.append(runoff_volume)
+
+    sum_runoff = ((max(Runoff) - 59.4817) / 59.4817) * 100
+    return sum_runoff
+
+def compute_peak_runoff(S1_BR, S1_1_BR, S2_BR, S2_1_BR, S3_BR, S3_1_BR, S4_BR, S4_1_BR, S5_BR, S5_1_BR,
+                   S6_BR, S6_1_BR, S6_2_BR, S7_BR, S7_1_BR, S8_BR, S9_BR, S10_BR, S10_1_BR, S11_BR,
+                   S11_1_BR, S12_BR, S12_1_BR, S12_2_BR, S12_3_BR, S12_4_BR, S13_BR, S13_1_BR, S14_BR, S14_1_BR):
+    sim = Simulation(r'../GuanyaoshanNJ.inp')
+    system_routing = SystemStats(sim)
+
+    prev_runoff = 0
+    Time_stamps = [] # add time series
+    Peak_Runoff = [] # creat a set for runoff data collection
+
+    lid_on_subs.__getitem__('S1').__getitem__(0).unit_area = S1_BR
+    lid_on_subs.__getitem__('S1-1').__getitem__(0).unit_area = S1_1_BR
+    lid_on_subs.__getitem__('S2').__getitem__(0).unit_area = S2_BR
+    lid_on_subs.__getitem__('S2-1').__getitem__(0).unit_area = S2_1_BR
+    lid_on_subs.__getitem__('S3').__getitem__(0).unit_area = S3_BR
+    lid_on_subs.__getitem__('S3-1').__getitem__(0).unit_area = S3_1_BR
+    lid_on_subs.__getitem__('S4').__getitem__(0).unit_area = S4_BR
+    lid_on_subs.__getitem__('S4-1').__getitem__(0).unit_area = S4_1_BR
+    lid_on_subs.__getitem__('S5').__getitem__(0).unit_area = S5_BR
+    lid_on_subs.__getitem__('S5-1').__getitem__(0).unit_area = S5_1_BR
+    lid_on_subs.__getitem__('S6').__getitem__(0).unit_area = S6_BR
+    lid_on_subs.__getitem__('S6-1').__getitem__(0).unit_area = S6_1_BR
+    lid_on_subs.__getitem__('S6-2').__getitem__(0).unit_area = S6_2_BR
+    lid_on_subs.__getitem__('S7').__getitem__(0).unit_area = S7_BR
+    lid_on_subs.__getitem__('S7-1').__getitem__(0).unit_area = S7_1_BR
+    lid_on_subs.__getitem__('S8').__getitem__(0).unit_area = S8_BR
+    lid_on_subs.__getitem__('S9').__getitem__(0).unit_area = S9_BR
+    lid_on_subs.__getitem__('S10').__getitem__(0).unit_area = S10_BR
+    lid_on_subs.__getitem__('S10-1').__getitem__(0).unit_area = S10_1_BR
+    lid_on_subs.__getitem__('S11').__getitem__(0).unit_area = S11_BR
+    lid_on_subs.__getitem__('S11-1').__getitem__(0).unit_area = S11_1_BR
+    lid_on_subs.__getitem__('S12').__getitem__(0).unit_area = S12_BR
+    lid_on_subs.__getitem__('S12-1').__getitem__(0).unit_area = S12_1_BR
+    lid_on_subs.__getitem__('S12-2').__getitem__(0).unit_area = S12_2_BR
+    lid_on_subs.__getitem__('S12-3').__getitem__(0).unit_area = S12_3_BR
+    lid_on_subs.__getitem__('S12-4').__getitem__(0).unit_area = S12_4_BR
+    lid_on_subs.__getitem__('S13').__getitem__(0).unit_area = S13_BR
+    lid_on_subs.__getitem__('S13-1').__getitem__(0).unit_area = S13_1_BR
+    lid_on_subs.__getitem__('S14').__getitem__(0).unit_area = S14_BR
+    lid_on_subs.__getitem__('S14-1').__getitem__(0).unit_area = S14_1_BR
+
+    for step in sim:
+        Time_stamps.append(sim.current_time)
+        runoff_stats = system_routing.runoff_stats
+        current_runoff = runoff_stats.__getitem__('runoff')
+        runoff_diff = current_runoff - prev_runoff
+
+        Peak_Runoff.append(runoff_diff)
+        prev_runoff = current_runoff
+
+    peak_runoff_reduction_rate = ((max(Peak_Runoff) - 0.7438) / 0.7438) * 100
+    return peak_runoff_reduction_rate
+
+problem = MyProblem(lid_on_subs)
+
+
+# choose algorithm
+from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.operators.crossover.sbx import SBX
+from pymoo.operators.mutation.pm import PM
+from pymoo.operators.sampling.rnd import FloatRandomSampling
+from pymoo.termination import get_termination
+
+algorithm = NSGA2(
+    pop_size=100,
+    n_offsprings=40,
+    sampling=FloatRandomSampling(),
+    crossover=SBX(prob=0.9, eta=15),
+    mutation=PM(eta=20),
+    eliminate_duplicates=True
+)
+
+
+termination = get_termination("n_gen", 30)
+
+from pymoo.optimize import minimize
+res = minimize(problem,
+               algorithm,
+               termination,
+               seed=1,
+               save_history=True,
+               verbose=True)
+
+X = res.X
+F = res.F
+
+
+import matplotlib.pyplot as plt
+from pymoo.visualization.scatter import Scatter
+
+# 3D Visualization
+plot = Scatter(tight_layout=True)
+plot.add(F, s=10)
+plot.show()
+
+# 2D Pairwise Scatter Plots
+plt.figure(figsize=(7, 5))
+plt.scatter(F[:, 0], F[:, 1], s=20, facecolors='none', edgecolors='blue')
+plt.title("Total runoff (y) and total cost (x)")
+plt.grid()
+plt.show()
+
+plt.scatter(F[:, 1], F[:, 2], s=20, facecolors='none', edgecolors='blue')
+plt.title("Peak volume (y) and Total runoff (x)")
+plt.grid()
+plt.show()
+
+plt.scatter(F[:, 0], F[:, 2], s=20, facecolors='none', edgecolors='blue')
+plt.title("Peak volume (y) and total cost (x)")
+plt.grid()
+plt.show()
+
+# save the data
+import pandas as pd
+result_df = pd.DataFrame(F)
+result_df.to_csv('output_objectives.csv', index=False)
+result_dx = pd.DataFrame(X)
+result_dx.to_csv('output_solutions.csv', index=False)
---
+++ 02_SWMM_optimization/03_solution_peak_volume.py
@@ -0,0 +1,137 @@
+'''
+PySWMM + Pymoo Code Practice
+A simple optimization case of LID (BR) area
+Objective: Min(total Cost) & Max(Reduction of runoff volume) & & Max(Reduction of peak volume)
+Author: Hanwen Xu
+Version: 1
+Date: Nov 01, 2023
+'''
+
+from pyswmm import Simulation, Subcatchments, LidGroups, SystemStats
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
+
+solutions_df = pd.read_csv('output_solutions.csv')
+fig = plt.figure(figsize=(12, 8))
+
+# 获取指定的解
+# solution_index = 99  # 可以修改为任意想要输出的解的索引
+# solution = solutions_df.iloc[solution_index]
+
+i = 1
+for index in range(0, 99):  # range的结束值是不包含的，所以这里是9而不是8
+    solution = solutions_df.iloc[index]
+
+    sim = Simulation(r'../GuanyaoshanNJ.inp')
+    lid_on_subs = LidGroups(sim)
+
+    lid_on_subs.__getitem__('S1').__getitem__(0).unit_area = solution[0]
+    lid_on_subs.__getitem__('S1-1').__getitem__(0).unit_area = solution[1]
+    lid_on_subs.__getitem__('S2').__getitem__(0).unit_area = solution[2]
+    lid_on_subs.__getitem__('S2-1').__getitem__(0).unit_area = solution[3]
+    lid_on_subs.__getitem__('S3').__getitem__(0).unit_area = solution[4]
+    lid_on_subs.__getitem__('S3-1').__getitem__(0).unit_area = solution[5]
+    lid_on_subs.__getitem__('S4').__getitem__(0).unit_area = solution[6]
+    lid_on_subs.__getitem__('S4-1').__getitem__(0).unit_area = solution[7]
+    lid_on_subs.__getitem__('S5').__getitem__(0).unit_area = solution[8]
+    lid_on_subs.__getitem__('S5-1').__getitem__(0).unit_area = solution[9]
+    lid_on_subs.__getitem__('S6').__getitem__(0).unit_area = solution[10]
+    lid_on_subs.__getitem__('S6-1').__getitem__(0).unit_area = solution[11]
+    lid_on_subs.__getitem__('S6-2').__getitem__(0).unit_area = solution[12]
+    lid_on_subs.__getitem__('S7').__getitem__(0).unit_area = solution[13]
+    lid_on_subs.__getitem__('S7-1').__getitem__(0).unit_area = solution[14]
+    lid_on_subs.__getitem__('S8').__getitem__(0).unit_area = solution[15]
+    lid_on_subs.__getitem__('S9').__getitem__(0).unit_area = solution[16]
+    lid_on_subs.__getitem__('S10').__getitem__(0).unit_area = solution[17]
+    lid_on_subs.__getitem__('S10-1').__getitem__(0).unit_area = solution[8]
+    lid_on_subs.__getitem__('S11').__getitem__(0).unit_area = solution[19]
+    lid_on_subs.__getitem__('S11-1').__getitem__(0).unit_area = solution[20]
+    lid_on_subs.__getitem__('S12').__getitem__(0).unit_area = solution[21]
+    lid_on_subs.__getitem__('S12-1').__getitem__(0).unit_area = solution[22]
+    lid_on_subs.__getitem__('S12-2').__getitem__(0).unit_area = solution[23]
+    lid_on_subs.__getitem__('S12-3').__getitem__(0).unit_area = solution[24]
+    lid_on_subs.__getitem__('S12-4').__getitem__(0).unit_area = solution[25]
+    lid_on_subs.__getitem__('S13').__getitem__(0).unit_area = solution[26]
+    lid_on_subs.__getitem__('S13-1').__getitem__(0).unit_area = solution[27]
+    lid_on_subs.__getitem__('S14').__getitem__(0).unit_area = solution[28]
+    lid_on_subs.__getitem__('S14-1').__getitem__(0).unit_area = solution[29]
+
+    system_routing = SystemStats(sim)
+    prev_runoff = 0
+    time_stamps = [] # add time series
+    Peak_Runoff = []
+
+    for step in sim:
+        time_stamps.append(sim.current_time)
+        runoff_stats = system_routing.runoff_stats
+        current_runoff = runoff_stats.__getitem__('runoff')
+        runoff_diff = current_runoff - prev_runoff
+        prev_runoff = current_runoff
+        Peak_Runoff.append(runoff_diff)
+
+    fig = plt.subplot(1, 1, 1)
+    fig.set_ylabel("Runoff volume (CMS)")  # label标签字号
+    fig.set_xlabel("Time")
+    fig.plot(time_stamps, Peak_Runoff, linestyle='-', label=f"solution{i}")
+    i = i + 1
+
+
+
+
+# default setting
+sim = Simulation(r'../GuanyaoshanNJ.inp')
+lid_on_subs = LidGroups(sim)
+
+lid_on_subs.__getitem__('S1').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S1-1').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S2').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S2-1').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S3').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S3-1').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S4').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S4-1').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S5').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S5-1').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S6').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S6-1').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S6-2').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S7').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S7-1').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S8').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S9').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S10').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S10-1').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S11').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S11-1').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S12').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S12-1').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S12-2').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S12-3').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S12-4').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S13').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S13-1').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S14').__getitem__(0).unit_area = 0.0
+lid_on_subs.__getitem__('S14-1').__getitem__(0).unit_area = 0.0
+
+system_routing = SystemStats(sim)
+prev_runoff = 0
+time_stamps = []
+original_peak_runoff_solution = []
+
+for step in sim:
+    time_stamps.append(sim.current_time)
+    runoff_stats = system_routing.runoff_stats
+    runoff = runoff_stats.__getitem__('runoff')
+    runoff_diff = runoff - prev_runoff
+    prev_runoff = runoff
+    original_peak_runoff_solution.append(runoff_diff)
+
+
+fig.plot(time_stamps, original_peak_runoff_solution, linestyle='-', label=f"default situation")
+fig.grid("xy")
+fig.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+fig.tick_params(axis='x', rotation=30)
+fig.legend()
+plt.show()
+sim.close()
